f0fcfb5c2f798e9ea95303cf0f98e588d4c88bf4
AniketGodse <149289805+AniketGodse@users.noreply.github.com>
2023-10-29 19:04:06 +0530

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,30 @@
+import random
+import string
+
+def generate_password(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+
+def main():
+    print("Password Generator")
+
+    try:
+        length = int(input("Enter password length: "))
+        if length <= 0:
+            print("Invalid length. Please enter a positive integer.")
+            return
+        
+
+        password = generate_password(length)
+        print("\nGenerated Password:")
+        print(password)
+
+
+    except ValueError:
+        print("Invalid input. Please enter a valid positive integer.")
+        
+
+if __name__ == "__main__":
+    main()
